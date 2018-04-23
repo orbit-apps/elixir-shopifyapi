3e9bbfce684ce27d2627d7915e892002ab477618
Hez Ronningen <code@hez.ca>
2018-04-23 15:53:33 -0700

    tests for "/authorized"

--- test/shopify_api/router_test.exs
+++ test/shopify_api/router_test.exs
@@ -12,7 +12,7 @@ defmodule ShopifyApi.RouterTest do
   @shop_domain "shop.example.com"
 
   setup do
-    ShopifyApi.AppServer.set(@app_name, %{name: @app_name, auth_redirect_uri: @redirect_uri})
+    ShopifyApi.AppServer.set(@app_name, %{name: @app_name, auth_redirect_uri: @redirect_uri, scope: "nothing"})
     ShopifyApi.ShopServer.set(%{domain: @shop_domain})
   end
 
@@ -42,14 +42,50 @@ defmodule ShopifyApi.RouterTest do
     end
   end
 
-  describe "/authoized" do
+  describe "/authorized" do
     @code "testing"
+    @token %{access_token: "test-token"}
 
-    test "fetches the token" do
+    setup _contxt do
+      bypass = Bypass.open()
+      shop_domain = "localhost:#{bypass.port}"
+      ShopifyApi.ShopServer.set(%{domain: shop_domain})
+
+      {:ok, %{bypass: bypass, shop_domain: shop_domain}}
+    end
+
+    test "fetches the token", %{bypass: bypass, shop_domain: shop_domain} do
+      Bypass.expect_once(bypass, "POST", "/admin/oauth/access_token", fn conn ->
+        {:ok, body} = Poison.encode(@token)
+        Plug.Conn.resp(conn, 200, body)
+      end)
+
       conn =
         conn(:get, "/authorized/#{@app_name}?shop=#{shop_domain}&code=#{@code}&timestamp=1234")
         |> parse
         |> ShopifyApi.Router.call(%{})
+
+      assert conn.status == 200
+      {:ok, %{token: auth_token}} = ShopifyApi.AuthTokenServer.get(shop_domain, @app_name)
+      assert auth_token == @token.access_token
+    end
+
+    test "fails without a valid app", %{bypass: bypass, shop_domain: shop_domain} do
+      conn =
+        conn(:get, "/authorized/invalid-app?shop=#{shop_domain}&code=#{@code}&timestamp=1234")
+        |> parse
+        |> ShopifyApi.Router.call(%{})
+
+      assert conn.status == 404
+    end
+
+    test "fails without a valid shop", %{bypass: bypass} do
+      conn =
+        conn(:get, "/authorized/#{@app_name}?shop=invalid-shop&code=#{@code}&timestamp=1234")
+        |> parse
+        |> ShopifyApi.Router.call(%{})
+
+      assert conn.status == 404
     end
   end
 end
