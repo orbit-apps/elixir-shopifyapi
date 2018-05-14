5efb9757be139a6f04af4d550ccb0afbe0d1ae97
Mike Gazdag <7515813+mikegazdag@users.noreply.github.com>
2018-05-14 14:44:27 -0700

    Add Shopify RESTful API Custom Saved Search endpoint. [CH7151]

---
+++ lib/shopify_api/rest/customer_saved_search.ex
@@ -0,0 +1,98 @@
+defmodule ShopifyApi.Rest.CustomerSavedSearch do
+  @moduledoc """
+  ShopifyApi REST API CustomerSavedSearch resource
+  """
+
+  require Logger
+
+  @doc """
+  Get a list of all the customer saved searches.
+
+  ## Example
+
+      iex> ShopifyApi.Rest.CustomerSavedSearch.all()
+      {:error, "Not implemented" }
+  """
+  def all() do
+    Logger.warn("#{__MODULE__} error, resource not implemented.")
+    {:error, "Not implemented"}
+  end
+
+  @doc """
+  Return a count of all the customer saved searches.
+
+  ## Example
+
+      iex> ShopifyApi.Rest.CustomerSavedSearch.count()
+      {:error, "Not implemented" }
+  """
+  def count() do
+    Logger.warn("#{__MODULE__} error, resource not implemented.")
+    {:error, "Not implemented"}
+  end
+
+  @doc """
+  Get a single customer saved searches.
+
+  ## Example
+
+      iex> ShopifyApi.Rest.CustomerSavedSearch.get()
+      {:error, "Not implemented" }
+  """
+  def get() do
+    Logger.warn("#{__MODULE__} error, resource not implemented.")
+    {:error, "Not implemented"}
+  end
+
+  @doc """
+  Retrieve all customers returned by a customer saved search.
+
+  ## Example
+
+      iex> ShopifyApi.Rest.CustomerSavedSearch.query()
+      {:error, "Not implemented" }
+  """
+  def query() do
+    Logger.warn("#{__MODULE__} error, resource not implemented.")
+    {:error, "Not implemented"}
+  end
+
+  @doc """
+  Create a customer saved search.
+
+  ## Example
+
+      iex> ShopifyApi.Rest.CustomerSavedSearch.create()
+      {:error, "Not implemented" }
+  """
+  def create() do
+    Logger.warn("#{__MODULE__} error, resource not implemented.")
+    {:error, "Not implemented"}
+  end
+
+  @doc """
+  Update a customer saved search.
+
+  ## Example
+
+      iex> ShopifyApi.Rest.CustomerSavedSearch.update()
+      {:error, "Not implemented" }
+  """
+  def update() do
+    Logger.warn("#{__MODULE__} error, resource not implemented.")
+    {:error, "Not implemented"}
+  end
+
+  @doc """
+  Delete a customer saved search.
+
+  ## Example
+
+      iex> ShopifyApi.Rest.CustomerSavedSearch.delete()
+      {:error, "Not implemented" }
+  """
+  def delete() do
+    Logger.warn("#{__MODULE__} error, resource not implemented.")
+    {:error, "Not implemented"}
+  end
+end
