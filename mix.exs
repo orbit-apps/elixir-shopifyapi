10f6eebbfe928e270c8b157ba7e5f74b4842a7b4
Hez Ronningen <code@hez.ca>
2018-05-25 09:50:41 -0700

    not used any more, will add back if we need it again

--- mix.exs
+++ mix.exs
@@ -45,7 +45,4 @@ defmodule Plug.ShopifyApi.MixProject do
       {:poison, "~> 3.1"}
     ]
   end
-
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
 end
