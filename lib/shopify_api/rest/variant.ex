21ac5d6272da158e7a2ee892b6d692873d8b2c30
Hez Ronningen <code@hez.ca>
2018-06-15 12:31:34 -0700

    Shopify refs this as variant

--- lib/shopify_api/rest/variant.ex
+++ lib/shopify_api/rest/variant.ex
@@ -1,4 +1,4 @@
-defmodule ShopifyAPI.REST.ProductVariant do
+defmodule ShopifyAPI.REST.Variant do
   @moduledoc """
   """
   alias ShopifyAPI.AuthToken
@@ -14,7 +14,7 @@ defmodule ShopifyAPI.REST.ProductVariant do
   @doc """
     Return all of a Product's Variants.
 
-    iex> ShopifyAPI.REST.ProductVariant.get(token, product_id)
+    iex> ShopifyAPI.REST.Variant.get(token, product_id)
 
   """
   def all(%AuthToken{} = auth, product_id) do
@@ -24,7 +24,7 @@ defmodule ShopifyAPI.REST.ProductVariant do
   @doc """
     Return a count of all Product Variants.
 
-  iex> ShopifyAPI.REST.ProductVariant.get(token, product_id)
+  iex> ShopifyAPI.REST.Variant.get(token, product_id)
   {:ok, %{"count" => integer}}
   """
 
@@ -35,7 +35,7 @@ defmodule ShopifyAPI.REST.ProductVariant do
   @doc """
     Delete a Product Variant.
 
-  iex> ShopifyAPI.REST.Product.delete(token, product_it, variant_id)
+  iex> ShopifyAPI.REST.Variant.delete(token, product_it, variant_id)
   {:ok, %{}}
   """
   def delete(%AuthToken{} = auth, product_id, variant_id) do
@@ -45,7 +45,7 @@ defmodule ShopifyAPI.REST.ProductVariant do
   @doc """
     Create a new Product Variant.
 
-  iex> ShopifyAPI.REST.ProductVariant.create(token, product_id, %{variant: %{body_html: "Testing variant create", title: "Testing Create Product Variant"}})
+  iex> ShopifyAPI.REST.Variant.create(token, product_id, %{variant: %{body_html: "Testing variant create", title: "Testing Create Product Variant"}})
   """
   def create(%AuthToken{} = auth, product_id, %{variant: %{}} = variant) do
     Request.post(auth, "products/#{product_id}/variants.json", variant)
@@ -54,7 +54,7 @@ defmodule ShopifyAPI.REST.ProductVariant do
   @doc """
     Update a Product Variant.
   """
-  def update(%AuthToken{} = auth, %{variant: %{variant_id: variant_id}} = variant) do
+  def update(%AuthToken{} = auth, %{variant: %{id: variant_id}} = variant) do
     Request.put(auth, "variants/#{variant_id}.json", variant)
   end
 end
