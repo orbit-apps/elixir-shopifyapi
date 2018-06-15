bea5439ec18fc5cc0cce60194ab18acf32928e41
Hez Ronningen <code@hez.ca>
2018-06-15 12:03:12 -0700

    fix outdate supervisor definition

--- lib/shopify_api/event_pipe/supervisor.ex
+++ lib/shopify_api/event_pipe/supervisor.ex
@@ -9,12 +9,8 @@ defmodule ShopifyAPI.EventPipe.Supervisor do
   end
 
   def init(_) do
-    Supervisor.init(
-      [
-        supervisor(WebhookEventQueue, []),
-        supervisor(Exq, [])
-      ],
-      strategy: :one_for_one
-    )
+    children = [WebhookEventQueue]
+
+    Supervisor.init(children, strategy: :one_for_one)
   end
 end
