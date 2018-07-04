de9123ebb7fdbf8225d949bf86d1ed65c6530c64
Daniel Insley <dan@insley.ca>
2018-07-04 15:57:12 -0700

    Cleaned up event queue duplication and had to bump version number up.

--- lib/shopify_api/event_pipe/event_queue.ex
+++ lib/shopify_api/event_pipe/event_queue.ex
@@ -2,25 +2,26 @@ defmodule ShopifyAPI.EventPipe.EventQueue do
   require Logger
 
   def enqueue(%{destination: :shopify, object: %{product: %{}}} = event) do
-    Logger.info(fn -> "Enqueueing #{inspect(event)}" end)
-    Exq.enqueue(Exq, "outbound", ShopifyAPI.EventPipe.ProductWorker, [event])
+    enqueue_event(ShopifyAPI.EventPipe.ProductWorker, event)
   end
 
   def enqueue(%{destination: :shopify, object: %{variant: %{}}} = event) do
-    Logger.info(fn -> "Enqueueing #{inspect(event)}" end)
-    Exq.enqueue(Exq, "outbound", ShopifyAPI.EventPipe.VariantWorker, [event])
+    enqueue_event(ShopifyAPI.EventPipe.VariantWorker, event)
   end
 
   def enqueue(%{destination: :shopify, object: %{inventory_level: %{}}} = event) do
-    Logger.info(fn -> "Enqueueing #{inspect(event)}" end)
-    Exq.enqueue(Exq, "outbound", ShopifyAPI.EventPipe.InventoryLevelWorker, [event])
+    enqueue_event(ShopifyAPI.EventPipe.InventoryLevelWorker, event)
   end
 
   def enqueue(%{destination: :shopify, object: %{location: %{}}} = event) do
-    Logger.info(fn -> "Enqueueing #{inspect(event)}" end)
-    Exq.enqueue(Exq, "outbound", ShopifyAPI.EventPipe.LocationWorker, [event])
+    enqueue_event(ShopifyAPI.EventPipe.LocationWorker, event)
   end
 
   def enqueue(event),
     do: Logger.warn("#{__MODULE__} does not know what worker should handle #{inspect(event)}")
+
+  defp enqueue_event(worker, event) do
+    Logger.info(fn -> "Enqueueing #{inspect(event)}" end)
+    Exq.enqueue(Exq, "outbound", worker, [event])
+  end
 end
