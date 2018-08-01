22d2f8820db268ae93796eb5b0476a6e01424c8f
Hez Ronningen <code@hez.ca>
2018-08-01 13:48:06 -0700

    simplify the workers, move the logging in to Event.execute_action/2

--- lib/shopify_api/event_pipe/inventory_level_worker.ex
+++ lib/shopify_api/event_pipe/inventory_level_worker.ex
@@ -7,8 +7,6 @@ defmodule ShopifyAPI.EventPipe.InventoryLevelWorker do
   alias ShopifyAPI.REST.InventoryLevel
 
   def perform(%{action: "set", object: _, token: _} = event) do
-    event
-    |> log
-    |> execute_action(fn token, %{object: level} -> InventoryLevel.set(token, level) end)
+    execute_action(event, fn token, %{object: level} -> InventoryLevel.set(token, level) end)
   end
 end
