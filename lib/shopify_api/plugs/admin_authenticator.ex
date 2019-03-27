e169b8e6ea2d292592c6f661e30c9cdf8ba22380
Hez Ronningen <code@hez.ca>
2019-03-27 09:00:28 -0700

    add session management and example + doc
    - previously it would only auth the first page load and then would fail as the user clicked through things.

--- lib/shopify_api/plugs/admin_authenticator.ex
+++ lib/shopify_api/plugs/admin_authenticator.ex
@@ -1,5 +1,24 @@
 defmodule ShopifyAPI.Plugs.AdminAuthenticator do
   @moduledoc """
+  The ShopifyAPI.Plugs.AdminAuthenticator plug allows for easy admin authentication. The plug when included
+  in your route will verify Shopify signatures, that are added to the iframe call on admin page load, and
+  set a session cookie for the duration of the session.
+
+  Make sure to include the App name in the path, in our example it is included directly in the path `"/shop-admin/:app"`.
+
+  ## Example Usage
+  ```elixir
+  # Router
+  pipeline :shop_admin do
+    plug ShopifyAPI.Plugs.AdminAuthenticator
+  end
+
+  scope "/shop-admin/:app", YourAppWeb do
+    pipe_through :browser
+    pipe_through :shop_admin
+    get "/", SomeAdminController, :index
+  end
+  ```
   """
   import Plug.Conn
   require Logger
@@ -12,22 +31,30 @@ defmodule ShopifyAPI.Plugs.AdminAuthenticator do
   alias ShopifyAPI.Security
   alias ShopifyAPI.ShopServer
 
+  @shopify_api_admin_authenticated :shopify_api_admin_authenticated
+
   def init(opts), do: opts
 
   def call(conn, options) do
-    with {:ok, app} <- ConnHelpers.fetch_shopify_app(conn),
-         true <- ConnHelpers.verify_params_with_hmac(app, conn.query_params) do
+    if get_sesion(conn, @shopify_api_admin_authenticated) do
       conn
-      |> ConnHelpers.assign_app()
-      |> ConnHelpers.assign_shop()
-      |> ConnHelpers.assign_auth_token()
     else
-      res ->
-        Logger.info("#{__MODULE__} failed authorized with: #{inspect(res)}")
-
+      with {:ok, app} <- ConnHelpers.fetch_shopify_app(conn),
+           true <- ConnHelpers.verify_params_with_hmac(app, conn.query_params) do
         conn
-        |> Conn.resp(401, "Not Authorized.")
-        |> Conn.halt()
+        |> ConnHelpers.assign_app()
+        |> ConnHelpers.assign_shop()
+        |> ConnHelpers.assign_auth_token()
+        |> put_session(conn, @shopify_api_admin_authenticated, true)
+      else
+        res ->
+          Logger.info("#{__MODULE__} failed authorized with: #{inspect(res)}")
+
+          conn
+          |> delete_session(conn, @shopify_api_admin_authenticated)
+          |> Conn.resp(401, "Not Authorized.")
+          |> Conn.halt()
+      end
     end
   end
 end
