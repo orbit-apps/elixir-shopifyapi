fc0917dbd76502f56ebc4f30852cad25677488e1
Hez Ronningen <code@hez.ca>
2019-05-13 09:27:45 -0700

    sort deps

--- mix.exs
+++ mix.exs
@@ -35,21 +35,23 @@ defmodule Plug.ShopifyAPI.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:absinthe_plug, "~> 1.4.0"},
-      {:absinthe, "~> 1.4.0"},
+      # Dev and Test
       {:bypass, "~> 1.0", only: :test},
       {:credo, "~> 1.0", only: [:dev, :test]},
       {:dialyxir, "~> 1.0.0-rc.6", only: [:dev], runtime: false},
-      {:excoveralls, ">= 0.0.0", only: [:dev, :test]},
       {:ex_doc, "~> 0.20.0", only: [:dev], runtime: false},
+      {:excoveralls, ">= 0.0.0", only: [:dev, :test]},
+      {:stream_data, "~> 0.4.2", only: :test},
+      # Everything else
+      {:absinthe, "~> 1.4.0"},
+      {:absinthe_plug, "~> 1.4.0"},
       {:exq, "~> 0.13.0"},
       {:exq_atomize_job_arguments, github: "pixelunion/exq-atomize-job-arguments", tag: "v0.1.2"},
       {:gen_stage, "~> 0.12"},
       {:httpoison, "~> 1.0"},
       {:jason, "~> 1.0"},
       {:plug, "~> 1.0"},
-      {:poison, "~> 3.1"},
-      {:stream_data, "~> 0.4.2", only: :test}
+      {:poison, "~> 3.1"}
     ]
   end
 
