d0ad78135733b287cd5c1820ca793501e5319ecb
Hez Ronningen <code@hez.ca>
2019-07-08 09:26:03 -0700

    formatting fixes

--- test/shopify_api/rest/tag_test.exs
+++ test/shopify_api/rest/tag_test.exs
@@ -6,32 +6,36 @@ defmodule ShopifyAPI.REST.TagTest do
 
   describe "Tags.encode and decode" do
     property "single tag test" do
-      check all tag_name <- string(:alphanumeric),
-                tag_name != "",
-                tag_value <- string(:alphanumeric),
-                tag_value != "" do
+      check all(
+              tag_name <- string(:alphanumeric),
+              tag_name != "",
+              tag_value <- string(:alphanumeric),
+              tag_value != ""
+            ) do
         assert [{tag_name, tag_value}] =
                  [{tag_name, tag_value}] |> Tag.encode("::") |> Tag.decode("::")
       end
     end
 
     property "tags collection" do
-      check all tags <- list_of({string(:alphanumeric), string(:alphanumeric)}) do
+      check all(tags <- list_of({string(:alphanumeric), string(:alphanumeric)})) do
         assert tags = tags |> Tag.encode("::") |> Tag.decode("::")
       end
     end
 
     property "mixed tags collection" do
-      check all complex_tags <- list_of({string(:alphanumeric), string(:alphanumeric)}),
-                complex_tags != [],
-                simple_tags <- list_of(string(:alphanumeric)) do
+      check all(
+              complex_tags <- list_of({string(:alphanumeric), string(:alphanumeric)}),
+              complex_tags != [],
+              simple_tags <- list_of(string(:alphanumeric))
+            ) do
         tags = complex_tags ++ simple_tags
         assert tags == tags |> Tag.encode("::") |> Tag.decode("::")
       end
     end
 
     property "simple tags" do
-      check all tags <- list_of(string(:alphanumeric)) do
+      check all(tags <- list_of(string(:alphanumeric))) do
         assert tags = tags |> Tag.encode() |> Tag.decode()
       end
     end
