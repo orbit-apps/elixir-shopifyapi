ceea5eb73b1ba11b869d3b3a829a2c874359a3ed
Hez Ronningen <code@hez.ca>
2019-09-20 09:09:44 -0700

    specs for Availability Tracker

--- lib/shopify_api/availability_tracker.ex
+++ lib/shopify_api/availability_tracker.ex
@@ -2,12 +2,44 @@ defmodule ShopifyAPI.AvailabilityTracker do
   @moduledoc """
   Handles Tracking of API throttling and when the API will be available for a request.
   """
+  alias HTTPoison.Response, as: HTTPResponse
   alias ShopifyAPI.{AuthToken, CallLimit}
 
+  @type available_count :: integer()
+  @type availability_delay :: integer()
+  @type t :: {available_count(), availability_delay()}
+
   @name :shopify_api_availability_tracker
+
+  @spec init() :: any()
+  def init, do: :ets.new(@name, [:named_table, :public])
+
+  @spec all() :: list()
   def all, do: :ets.tab2list(@name)
 
-  @spec get(AuthToken.t(), DateTime.t()) :: {integer(), integer()}
+  @spec api_hit_limit(AuthToken.t(), HTTPResponse.t(), DateTime.t()) :: t()
+  def api_hit_limit(%AuthToken{} = token, http_response, now \\ DateTime.utc_now()) do
+    available_modifier =
+      http_response
+      |> CallLimit.get_retry_after_header()
+      |> CallLimit.get_retry_after_milliseconds()
+
+    set(token, 0, available_modifier, now)
+  end
+
+  @spec update_api_call_limit(AuthToken.t(), HTTPResponse.t()) :: t()
+  def update_api_call_limit(%AuthToken{} = token, http_response) do
+    http_response
+    |> CallLimit.limit_header_or_status_code()
+    |> CallLimit.get_api_remaining_calls()
+    |> case do
+      # Wait for a second to allow time for a bucket fill
+      0 -> set(token, 0, 1_000)
+      remaining -> set(token, remaining, 0)
+    end
+  end
+
+  @spec get(AuthToken.t(), DateTime.t()) :: t()
   def get(%AuthToken{} = token, now \\ DateTime.utc_now()) do
     case :ets.lookup(@name, AuthToken.create_key(token)) do
       [] ->
@@ -20,6 +52,8 @@ defmodule ShopifyAPI.AvailabilityTracker do
     end
   end
 
+  @spec set(AuthToken.t(), available_count(), availability_delay(), DateTime.t()) ::
+          t()
   def set(token, available_count, availability_delay, now \\ DateTime.utc_now())
 
   # Do nothing
@@ -36,29 +70,5 @@ defmodule ShopifyAPI.AvailabilityTracker do
 
     :ets.insert(@name, {AuthToken.create_key(token), available_count, available_at})
     {available_count, availability_delay}
-  end
-
-  def api_hit_limit(%AuthToken{} = token, http_response, now \\ DateTime.utc_now()) do
-    available_modifier =
-      http_response
-      |> CallLimit.get_retry_after_header()
-      |> CallLimit.get_retry_after_milliseconds()
-
-    set(token, 0, available_modifier, now)
-  end
-
-  def update_api_call_limit(%AuthToken{} = token, http_response) do
-    http_response
-    |> CallLimit.limit_header_or_status_code()
-    |> CallLimit.get_api_remaining_calls()
-    |> case do
-      # Wait for a second to allow time for a bucket fill
-      0 -> set(token, 0, 1_000)
-      remaining -> set(token, remaining, 0)
-    end
-  end
-
-  def init do
-    :ets.new(@name, [:named_table, :public])
   end
 end
