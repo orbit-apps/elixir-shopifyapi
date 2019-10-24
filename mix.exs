f14c235d23f6c92759c5a52f7e410cd17e37f996
Hez Ronningen <code@hez.ca>
2019-10-24 09:05:34 -0700

    Bumping elixir to 1.9 and erlang to 22

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Plug.ShopifyAPI.MixProject do
     [
       app: :shopify_api,
       version: @version,
-      elixir: "~> 1.7",
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [plt_add_deps: :transitive, plt_file: {:no_warn, "priv/plts/dialyzer.plt"}],
