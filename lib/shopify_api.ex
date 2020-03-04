931033aaea3551a0f19b53b9245bacadf18893d1
Jason Freeman <jason.freeman@pixelunion.net>
2020-03-04 12:50:43 -0800

    Add doc for ShopifyAPI.graphql_request/5

--- lib/shopify_api.ex
+++ lib/shopify_api.ex
@@ -2,6 +2,18 @@ defmodule ShopifyAPI do
   alias ShopifyAPI.RateLimiting
   alias ShopifyAPI.Throttled
 
+  @doc """
+  A helper function for making throttled GraphQL requests.
+
+  ## Example:
+
+      iex> query = "mutation metafieldDelete($input: MetafieldDeleteInput!){ metafieldDelete(input: $input) {deletedId userErrors {field message }}}",
+      iex> estimated_cost = 10
+      iex> variables = %{input: %{id: "gid://shopify/Metafield/9208558682200"}}
+      iex> options = [debug: true]
+      iex> ShopifyAPI.graphql_request(auth_token, query, estimated_cost, variables, options)
+      {:ok, %ShopifyAPI.GraphQL.Response{...}}
+  """
   @spec graphql_request(ShopifyAPI.AuthToken.t(), String.t(), integer(), map(), list()) ::
           ShopifyAPI.GraphQL.query_response()
   def graphql_request(token, query, estimated_cost, variables \\ %{}, opts \\ []) do
