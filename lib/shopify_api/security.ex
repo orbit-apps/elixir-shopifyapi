83c9117545d16b19c21e0b66dd92c7a988b488d7
Hez Ronningen <code@hez.ca>
2021-06-07 15:10:08 -0700

    Move hmac in to a otp dependant call
    :crypto.hmac is not in otp24, this gives us backwards compatibility for as long as we need it.

--- lib/shopify_api/security.ex
+++ lib/shopify_api/security.ex
@@ -1,14 +1,21 @@
 defmodule ShopifyAPI.Security do
   def base16_sha256_hmac(text, secret) do
     :sha256
-    |> :crypto.hmac(secret, text)
+    |> hmac(secret, text)
     |> Base.encode16()
     |> String.downcase()
   end
 
   def base64_sha256_hmac(text, secret) do
     :sha256
-    |> :crypto.hmac(secret, text)
+    |> hmac(secret, text)
     |> Base.encode64()
+  end
+
+  # TODO: remove when we require OTP 22
+  if System.otp_release() >= "22" do
+    defp hmac(digest, key, data), do: :crypto.mac(:hmac, digest, key, data)
+  else
+    defp hmac(digest, key, data), do: :crypto.hmac(digest, key, data)
   end
 end
