b19ac1655b4bb535c5f79cb43387a16a555d3f92
Hez Ronningen <code@hez.ca>
2025-02-19 08:57:35 -0800

    set min ver to what we test in ci

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Plug.ShopifyAPI.MixProject do
     [
       app: :shopify_api,
       version: @version,
-      elixir: "~> 1.13",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       dialyzer: [
