92a1ce24d568d454760b507c073c21b47e7127b6
Hez Ronningen <code@hez.ca>
2025-02-27 10:54:04 -0800

    refactor auth shop session to use new JWT module

--- lib/shopify_api/plugs/auth_shop_session_token.ex
+++ lib/shopify_api/plugs/auth_shop_session_token.ex
@@ -2,9 +2,6 @@ defmodule ShopifyAPI.Plugs.AuthShopSessionToken do
   @moduledoc """
   A Plug to handle authenticating the Shop Admin JWT from Shopify.
 
-  Options
-    - use_online_tokens (boolean) default: false
-
   ## Example Installations
 
   Add this plug in a pipeline for your Shop Admin API.
@@ -15,42 +12,27 @@ defmodule ShopifyAPI.Plugs.AuthShopSessionToken do
     plug ShopifyAPI.Plugs.AuthShopSessionToken
   end
   ```
-
-  If you want to use online tokens (per user tokens), you will need to enable it here
-
-  ```elixir
-  pipeline :shop_admin_api do
-    plug :accepts, ["json"]
-    plug ShopifyAPI.Plugs.AuthShopSessionToken, use_online_tokens: true
-  end
-  ```
   """
 
   import Plug.Conn
 
   require Logger
 
-  alias ShopifyAPI.AppServer
   alias ShopifyAPI.AuthTokenServer
+  alias ShopifyAPI.JWTSessionToken
   alias ShopifyAPI.ShopServer
-  alias ShopifyAPI.UserTokenServer
 
-  @defaults [use_online_tokens: false]
-  @client_redirect_header "x-shopify-api-request-failure-reauthorize-url"
+  def init(opts), do: opts
 
-  def init(opts), do: Keyword.merge(@defaults, opts)
-
-  def call(conn, options) do
-    use_online_tokens = options[:use_online_tokens]
-
+  def call(conn, _options) do
     with ["Bearer " <> token] <- get_req_header(conn, "authorization"),
-         {:ok, app} <- get_app_from_token(token),
-         {true, jwt, _jws} <- verify_token(token, app.client_secret),
-         {:ok, myshopify_domain} <- get_my_shopify_domain_from_jwt(jwt),
-         {:ok, user_id} <- get_user_from_jwt(jwt),
+         {:ok, app} <- JWTSessionToken.app(token),
+         {true, jwt, _jws} <- JWTSessionToken.verify(token, app.client_secret),
+         {:ok, myshopify_domain} <- JWTSessionToken.myshopify_domain(jwt),
+         {:ok, user_id} <- JWTSessionToken.user_id(jwt),
          {:ok, shop} <- ShopServer.get(myshopify_domain),
          {:ok, auth_token} <- AuthTokenServer.get(myshopify_domain, app.name),
-         {:ok, user_token} <- get_user_token(myshopify_domain, app, user_id, use_online_tokens) do
+         {:ok, user_token} <- JWTSessionToken.get_user_token(jwt, token) do
       conn
       |> assign(:app, app)
       |> assign(:shop, shop)
@@ -58,14 +40,6 @@ defmodule ShopifyAPI.Plugs.AuthShopSessionToken do
       |> assign(:user_id, user_id)
       |> assign(:user_token, user_token)
     else
-      {:error, :user_token_invalid, redirect_url} ->
-        Logger.debug("Found invalid user token adding redirect header to #{redirect_url}")
-
-        conn
-        |> resp(403, "Not Authorized.")
-        |> put_resp_header(@client_redirect_header, redirect_url)
-        |> halt()
-
       error ->
         Logger.debug("Could not authenticate shop #{inspect(error)}")
 
@@ -74,45 +48,4 @@ defmodule ShopifyAPI.Plugs.AuthShopSessionToken do
         |> halt()
     end
   end
-
-  defp verify_token(token, client_secret) do
-    jwk = JOSE.JWK.from_oct(client_secret)
-    JOSE.JWT.verify_strict(jwk, ["HS256"], token)
-  end
-
-  defp get_user_token(_domain, _app, _user_id, false), do: {:ok, nil}
-
-  defp get_user_token(domain, app, id, true) do
-    case UserTokenServer.get_valid(domain, app.name, id) do
-      {:ok, token} ->
-        {:ok, token}
-
-      {:error, _} = resp ->
-        Logger.debug("Get user token failed with #{inspect(resp)}")
-        oauth_url = ShopifyAPI.shopify_oauth_url(app, domain, use_user_tokens: true)
-        {:error, :user_token_invalid, oauth_url}
-    end
-  end
-
-  defp get_app_from_token(token) do
-    with %JOSE.JWT{fields: %{"aud" => client_id}} <- JOSE.JWT.peek_payload(token),
-         {:ok, app} <- AppServer.get_by_client_id(client_id) do
-      {:ok, app}
-    else
-      _ -> {:error, "Audience claim is not a valid App clientId."}
-    end
-  end
-
-  defp get_my_shopify_domain_from_jwt(%JOSE.JWT{fields: %{"dest" => shop_url}}) do
-    shop_url
-    |> URI.parse()
-    |> Map.get(:host)
-    |> case do
-      shop_name when is_binary(shop_name) -> {:ok, shop_name}
-      _ -> {:error, "Shop name not found"}
-    end
-  end
-
-  defp get_user_from_jwt(%JOSE.JWT{fields: %{"sub" => user_id}}),
-    do: {:ok, String.to_integer(user_id)}
 end
